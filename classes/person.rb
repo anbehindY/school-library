2c9b2ab100afd3e7b6d50dedb37da55218d934bb
anbehindY <yanminthwin@gmail.com>
2023-06-27 20:10:08 +0630

    Check actions of decorators

--- classes/person.rb
+++ classes/person.rb
@@ -1,4 +1,5 @@
 require_relative 'nameable'
+require_relative 'decorator'
 
 class Person < Nameable
   attr_accessor :name, :age
@@ -15,13 +16,20 @@ class Person < Nameable
     @parent_permission || of_age?
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
-
-  def correct_name
-    @name
-  end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
