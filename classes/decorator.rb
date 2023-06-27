378233ca13022acb70b493db317116fda57a06d7
anbehindY <yanminthwin@gmail.com>
2023-06-27 20:01:25 +0630

    Prepare capitalize and trimmer decorators

--- classes/decorator.rb
+++ classes/decorator.rb
@@ -11,3 +11,20 @@ attr_accessor :nameable
     @nameable.correct_name
   end
 end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    word = @nameable.correct_name
+    if word.length > 10
+      word.slice(0, 10)
+    else
+      word
+    end
+  end
+end
