9aac5349977c5d9f3c03a66533082efeb699cb6a
anbehindY <yanminthwin@gmail.com>
2023-06-29 08:28:41 +0630

    Connect book and person classes

--- classes/person.rb
+++ classes/person.rb
@@ -3,14 +3,20 @@ require_relative 'decorator'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.person = self
   end
 
   def can_use_services?
