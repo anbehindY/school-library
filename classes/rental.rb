904d63878de67b67fa5e6e5f08bc916abc14b5ce
anbehindY <yanminthwin@gmail.com>
2023-06-29 08:20:56 +0630

    connect book and rental classes

--- classes/rental.rb
+++ classes/rental.rb
@@ -1,7 +1,12 @@
 class Rental
   attr_accessor :date
+  attr_reader :book
 
   def initialize(date)
     @date = date
   end
+
+  def book=(book)
+    @book = book
+    book.add_rental(self) unless book.rentals.include?(self)
 end
