95dfb8baa283339cb5f0b8e75a5a715153f573b3
anbehindY <yanminthwin@gmail.com>
2023-06-30 20:07:29 +0630

    Fix rubocop error

--- app.rb
+++ app.rb
@@ -20,8 +20,12 @@ class App
   def list_people
     puts 'No people found' if @people.empty?
     @people.each do |person|
-      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" if person.instance_of?(Student)
-      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" if person.instance_of?(Teacher)
+      if person.instance_of?(Student)
+        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+      if person.instance_of?(Teacher)
+        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
     end
   end
 
