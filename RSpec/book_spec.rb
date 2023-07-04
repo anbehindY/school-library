d0d8bea99053989e8d59d453879f79ff30197b2c
anbehindY <yanminthwin@gmail.com>
2023-07-04 22:05:36 +0630

    Add test for book.rb

---
+++ RSpec/book_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../classes/book'
+
+describe Book do
+  context 'After typing in title and author of the book' do
+    new_book1 = Book.new('Harry Potter', 'J.K. Rowling')
+    it 'should create the book' do
+      expect(new_book1.title).to eq('Harry Potter')
+      expect(new_book1.author).to eq('J.K. Rowling')
+    end
+  end
+
+  context 'After choosing book and person' do
+    new_book2 = Book.new('Game of Thrones', 'George R. R. Martin')
+    new_book2.add_rental('2021-01-01', 'John Doe')
+    new_book2.add_rental('2021-01-02', 'Jane Doe')
+    it 'should create a rental and add it to the rentals array' do
+      expect(new_book2.rentals.length).to eq 2
+      expect(new_book2.rentals[0].date).to eq('2021-01-01')
+      expect(new_book2.rentals[1].date).to eq('2021-01-02')
+    end
+  end
+end
